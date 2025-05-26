bffea0b4cff0f04a5ea2b6ade89990a219e866a3
DudekulaAbeed <abeeddudekula999@gmail.com>
2025-05-26 10:35:59 +0530

    life created

---
+++ 01_variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=KHOLI
+PERSON2=MODI
+
+echo "$PERSON1:finally i am taking retirement from test cricket"
+echo "$PERSON2:oh,$PERSON1 why don't take that back"
+echo "$PERSON1:no,pm ji reason is to encourage young team"
+echo "$PERSON2:what a man $PERSON1 you are, really a true inspiration to all, enjoy your free with your family and thanks for leading india the best in test cricket happy retirement legend"
+echo "$PERSON1:thank you $PERSON2 PM sir for your kind words"
